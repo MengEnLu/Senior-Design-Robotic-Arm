3fc5a9e5fe069585865c6c91097710ffa6e159a7
MengEnLu <mlu012@ucr.edu>
2020-01-30 02:36:09 -0800

    Updated extract data function

--- test_bluetooth_mega_slave/test_bluetooth_mega_slave.ino
+++ test_bluetooth_mega_slave/test_bluetooth_mega_slave.ino
@@ -1,6 +1,18 @@
 //Mega Serial 1 TX1 pin 18
 //Mega Serial 1 RX1 pin 19
+int dataArray[] ={0,0,0,0,0,0,0,0,0,0};
+int thumb_pos =dataArray[0];
+int index_pos = dataArray[1];
+int middle_pos = dataArray[2];
+int rp_pos = dataArray[3];
 
+int gy1_x = dataArray[4];
+int gy1_y = dataArray[5];
+int gy1_z = dataArray[6];
+
+int gy2_x = dataArray[7];
+int gy2_y = dataArray[8];
+int gy2_z = dataArray[9];
 
 void setup() {
   // put your setup code here, to run once:
@@ -12,10 +24,6 @@ void setup() {
   bluetoothSetUp();
   Serial.print("Ready");
 
-  //test
-  //String temp = "<111\t200\t\333\t\444\t\555\t666\t777\t888\t999\t000>";
-  //Serial1.print(temp);
-  
 }
 
 void loop() {
@@ -35,11 +43,20 @@ void loop() {
     }
   }
   //test
-  Serial.print("data String Ready: ");
-  Serial.println(dataStr);
+  //Serial.print("data String Ready: ");
+  //Serial.println(dataStr);
   
   //TO DO LIST:
   //INSERT FUNCTIONS TO EXTRACT DATA
+  extractData(dataStr);
+  assignVar();
+  
+  //test extract Data function
+  test_Print_Extracted_Data();
+  
+  
+  
+   //TO DO LIST:
   //INSERT FUNCTIONS FOR SERVOS AND MOTOR HERE
 
   //test delay is set to 1.5 sec
@@ -61,7 +78,6 @@ String receive(){
 
   String str = "";
   bool flag = false;
-  //char c;
 
   while(flag == false){
     String tempStr ="";
@@ -74,17 +90,8 @@ String receive(){
       str = tempStr;
       flag = true;
     }
-   
   }
-
   
-  /*while(i<64){
-    if(Serial1.available()){
-      tempStr += Serial1.read();
-      i++;
-    }
-  }
-  */
   return str;
 }
 
@@ -102,18 +109,108 @@ bool findDataStr(String tempStr){
 String getDataStr(String tempStr){
   int i = tempStr.indexOf('<');
   tempStr = tempStr.substring(i);
-  int k = tempStr.indexOf('>');
+  tempStr = tempStr + '>';
+  
+  /*int k = tempStr.indexOf('>');
+        //test
+  Serial.print("tempStr.length()-1: ");
+  Serial.println(tempStr.length()-1);
+  
   if (k <= tempStr.length()-1){
     tempStr = tempStr.substring(0,k+1);
   }
-  else{
+  else{    
     tempStr = tempStr.substring(0,k);
     tempStr = tempStr + '>';
   }
-
+  */
   return tempStr;
 }
 
+void assignVar(){
+  thumb_pos =dataArray[0];
+  index_pos = dataArray[1];
+  middle_pos = dataArray[2];
+  rp_pos = dataArray[3];
+
+  gy1_x = dataArray[4];
+  gy1_y = dataArray[5];
+  gy1_z = dataArray[6];
+
+  gy2_x = dataArray[7];
+  gy2_y = dataArray[8];
+  gy2_z = dataArray[9];
+}
+
+void extractData(String dataStr){
+  //int tempArray[10];
+  int start = 1;
+  int e = -1;
+  int counter = 0;
+  //Serial.print("tempArray Size");
+  //Serial.println(sizeof(tempArray));
+  for(int i = 1; i <= dataStr.length()-1; i++){
+    if((dataStr.charAt(i) == '\t') || (dataStr.charAt(i) == '>')){
+      e = i;
+      String temp = dataStr.substring(start,e);
+      //test
+      //Serial.println(temp);
+      dataArray[counter] = temp.toInt();
+      //test
+      /*Serial.print("counter\t");
+      Serial.print(counter);
+      Serial.print("\tdataArrayVal:\t");
+      Serial.println(dataArray[counter]);
+      */   
+      counter++;
+      start = i+1;
+      //test
+      //Serial.println(i);
+    }
+  }
+  //Serial.print("tempArray Size: ");
+  //Serial.println(sizeof(tempArray));
+}
+
+
+void assignVal(){
+   thumb_pos =dataArray[0];
+   index_pos = dataArray[1];
+   middle_pos = dataArray[2];
+   rp_pos = dataArray[3];
+
+   gy1_x = dataArray[4];
+   gy1_y = dataArray[5];
+   gy1_z = dataArray[6];
+
+   gy2_x = dataArray[7];
+   gy2_y = dataArray[8];
+   gy2_z = dataArray[9];
+}
+
+
+void test_Print_Extracted_Data(){
+  Serial.print("thumb: ");
+  Serial.print(thumb_pos);
+  Serial.print("\tindex: ");
+  Serial.print(index_pos);
+  Serial.print("\tmiddle: ");
+  Serial.print(middle_pos);
+  Serial.print("\trp: ");
+  Serial.print(rp_pos);
+  Serial.print("\tgy1_x: ");
+  Serial.print(gy1_x);
+  Serial.print("\tgy1_y: ");
+  Serial.print(gy1_y);
+  Serial.print("\tgy1_z: ");
+  Serial.print(gy1_z);
+  Serial.print("\tgy2_x: ");
+  Serial.print(gy2_x);
+  Serial.print("\tgy2_y: ");
+  Serial.print(gy2_y);
+  Serial.print("\tgy2_z: ");
+  Serial.println(gy2_z);
+}
 
 
 void clearBuffer(){
